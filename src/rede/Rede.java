664f95eb5e58796cf408e7c25922acb6777c7e0d
oaguado <oaguado@192.168.56.1>
2022-03-09 13:56:20 +0100

    Enunciado Exercicio

---
+++ src/rede/Rede.java
@@ -0,0 +1,25 @@
+package rede;
+
+/**
+ *
+ * @author Óscar Núñez Aguado <aguado@edu.xunta.es>
+ */
+public class Rede {
+
+    public static void main(String[] args) {
+        // Declaración de variables y constantes
+
+        // Entrada de datos
+        // Proceso
+        // Salida
+    }
+
+    public static boolean validarIP(String ip) {
+        return false;
+    }
+
+    public static int buscarIP(String ip, String[] ips) {
+        return 0;
+    }
+
+}
