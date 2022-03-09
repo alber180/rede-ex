17852ea455a0937eae2a3406312b07cfe4360b77
oaguado <oaguado@192.168.56.1>
2022-03-09 14:00:09 +0100

    Enunciado Exercicio

---
+++ test/rede/RedeTest.java
@@ -0,0 +1,47 @@
+package rede;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Óscar Núñez Aguado <aguado@edu.xunta.es>
+ */
+public class RedeTest {
+
+    public RedeTest() {
+    }
+
+    @Test
+    public void validarIP() {
+        assertTrue(Rede.validarIP("192.168.0.9"));
+        assertTrue(Rede.validarIP("192.168.100.0"));
+        assertTrue(Rede.validarIP("255.255.255.255"));
+        assertTrue(Rede.validarIP("0.0.0.0"));
+
+        assertFalse(Rede.validarIP("..."));
+        assertFalse(Rede.validarIP("192.168..9"));
+        assertFalse(Rede.validarIP("192.168:0.9"));
+        assertFalse(Rede.validarIP("192.168.0.009"));
+        assertFalse(Rede.validarIP("192.168.0.9."));
+        assertFalse(Rede.validarIP("256.0.0.0"));
+        assertFalse(Rede.validarIP("255.0.0.0"));
+    }
+
+    @Test
+    public void buscarIP() {
+        String[] ips = {"192.168.0.9", "192.168.100.0", "255.255.255.255", "0.0.0.0"};
+        String[] vacio = new String[0];
+
+        assertEquals(Rede.buscarIP("192.168.0.9", ips), 0);
+        assertEquals(Rede.buscarIP("192.168.100.0", ips), 1);
+        assertEquals(Rede.buscarIP("255.255.255.255", ips), 2);
+        assertEquals(Rede.buscarIP("0.0.0.0", ips), 3);
+
+        assertEquals(Rede.buscarIP("192.168.100.9", ips), -1);
+        assertEquals(Rede.buscarIP("192.168.0.009", ips), -1);
+        assertEquals(Rede.buscarIP("", ips), -1);
+        assertEquals(Rede.buscarIP("0.0.0.0", vacio), -1);
+    }
+
+}
